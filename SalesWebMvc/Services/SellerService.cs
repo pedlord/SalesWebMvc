bc41cfcf27a61501d678ba8b056f8f07a9928a3f
Pedro Inacio <pedroinacio1939@gmail.com>
2023-02-13 22:12:32 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,7 +17,7 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
         public void Insert(Seller obj) {
-            obj.Department = _context.Department.First();
+            //obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
