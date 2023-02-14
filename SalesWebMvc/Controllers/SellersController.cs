3eeed48dae94a412a4b6be5fdbe077c5b7996ae6
Pedro Inacio <pedroinacio1939@gmail.com>
2023-02-13 23:13:34 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -2,6 +2,8 @@
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
+using SalesWebMvc.Services.Exceptions;
+using System.Collections.Generic;
 
 namespace SalesWebMvc.Controllers
 {
@@ -67,6 +69,42 @@ namespace SalesWebMvc.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);  //due to the chance of it been nullabe, its necessary to use "value"
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);  //due to the chance of it been nullabe, its necessary to use "value"
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException) {
+                return NotFound();
+            }
+            catch (DbConcurrencyException) {
+                return BadRequest();
+            }
         }
     }
 }
